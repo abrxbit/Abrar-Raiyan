210e1e4b95393f55da7b3bf2c307fd743e456f19
Abrar Raiyan <abrarraiyan36@gmail.com>
2025-11-29 10:47:11 +0600

    Create Problem 116 – Decimal to Hexadecimal Conversion .c

---
+++ Problem 116 – Decimal to Hexadecimal Conversion .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    char hex[100];
+    int i=0;
+    while(n>0){
+        int r=n%16;
+        if(r<10) hex[i++]='0'+r;
+        else hex[i++]='A'+(r-10);
+        n/=16;
+    }
+    for(int j=i-1;j>=0;j--) printf("%c", hex[j]);
+    return 0;
+}
