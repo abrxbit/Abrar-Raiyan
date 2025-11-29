b6546375d496302945553c025d3143b78bfe97fe
Abrar Raiyan <abrarraiyan36@gmail.com>
2025-11-29 17:49:13 +0600

    Create Problem 146 – Display Dates of Calendar Year in Different Formats .c

---
+++ Problem 146 – Display Dates of Calendar Year in Different Formats .c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int d,m,y;
+    scanf("%d %d %d",&d,&m,&y);
+    printf("%02d-%02d-%04d\n", d,m,y);
+    printf("%02d/%02d/%04d\n", d,m,y);
+    printf("%04d/%02d/%02d\n", y,m,d);
+    return 0;
+}
