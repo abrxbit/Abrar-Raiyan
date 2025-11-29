be1258cd49b2e9c7818643b68e741e84b718b64b
Abrar Raiyan <abrarraiyan36@gmail.com>
2025-11-29 11:04:25 +0600

    Create Problem 124 – Sort 2D Array of Strings .c

---
+++ Problem 124 – Sort 2D Array of Strings .c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    char s[n][100];
+    for(int i=0;i<n;i++) scanf("%s", s[i]);
+    for(int i=0;i<n-1;i++)
+        for(int j=i+1;j<n;j++)
+            if(strcmp(s[i],s[j])>0){
+                char t[100];
+                strcpy(t,s[i]);
+                strcpy(s[i],s[j]);
+                strcpy(s[j],t);
+            }
+    for(int i=0;i<n;i++) printf("%s\n", s[i]);
+    return 0;
+}
